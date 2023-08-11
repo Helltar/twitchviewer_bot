6c792389c2ec69096d4efe31484549e506ef9f2e
Helltar <iam@helltar.com>
2023-08-11 20:36:55 +0300

    update Commands (const to uppercase)

--- src/main/kotlin/com/helltar/twitchviewer_bot/commands/Commands.kt
+++ src/main/kotlin/com/helltar/twitchviewer_bot/commands/Commands.kt
@@ -6,14 +6,14 @@ import com.helltar.twitchviewer_bot.keyboard.BtnCallbacks.BUTTON_SCREENSHOT
 
 object Commands {
 
-    const val commandAdd = "cmdAdd"
-    const val commandList = "cmdList"
-    const val commandStart = "cmdStart"
-    const val commandUptime = "cmdUptime"
-    const val commandAbout = "cmdAbout"
-    const val commandClipCompress = "cmdCompress"
+    const val COMMAND_ADD = "cmdAdd"
+    const val COMMAND_LIST = "cmdList"
+    const val COMMAND_START = "cmdStart"
+    const val COMMAND_UPTIME = "cmdUptime"
+    const val COMMAND_ABOUT = "cmdAbout"
+    const val COMMAND_CLIP_COMPRESS = "cmdCompress"
 
-    const val commandClip = BUTTON_CLIPS
-    const val commandLive = BUTTON_LIVE
-    const val commandScreenshot = BUTTON_SCREENSHOT
+    const val COMMAND_CLIP = BUTTON_CLIPS
+    const val COMMAND_LIVE = BUTTON_LIVE
+    const val COMMAND_SCREENSHOT = BUTTON_SCREENSHOT
 }
