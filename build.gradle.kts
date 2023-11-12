3f5f2d5b62db31cdf7493743fe001cc464ff54c9
Helltar <iam@helltar.com>
2023-11-12 13:58:59 +0200

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     application
 }
 
@@ -13,17 +13,17 @@ repositories {
 dependencies {
     implementation ("com.annimon:tgbots-module:6.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.3")
-    implementation("com.github.twitch4j:twitch4j:1.17.0")
+    implementation("com.github.twitch4j:twitch4j:1.18.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("ch.qos.logback:logback-classic:1.4.7")
 
     implementation("com.h2database:h2:2.1.214")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.44.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.44.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.44.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.44.1")
 }
 
 application {
