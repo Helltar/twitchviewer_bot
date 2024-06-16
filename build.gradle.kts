941b677e97266ec9152531012577e5895af2c20d
Helltar <iam@helltar.com>
2024-06-16 17:29:18 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,34 @@
+val exposed_version: String by project
+val postgresql_driver_version: String by project
+
 plugins {
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
 group = "com.helltar"
-version = "0.3.1"
+version = "0.7.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation ("com.annimon:tgbots-module:7.1.0")
+    implementation("com.annimon:tgbots-module:7.3.9")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
-    implementation("com.github.twitch4j:twitch4j:1.18.0")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.github.twitch4j:twitch4j:1.20.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
 
-    implementation("com.h2database:h2:2.1.214")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.50.1")
-    implementation("org.jetbrains.exposed:exposed-core:0.50.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.50.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.50.1")
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
-    implementation("org.slf4j:slf4j-api:2.0.6")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 application {
