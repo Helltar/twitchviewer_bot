f7df534fc3d751e473558f8b7cc659e890293b35
Helltar <iam@helltar.com>
2025-01-22 00:40:29 +0200

    refactor command execution method calls

--- src/main/kotlin/com/helltar/twitchviewerbot/bot/TwitchViewerBotHandler.kt
+++ src/main/kotlin/com/helltar/twitchviewerbot/bot/TwitchViewerBotHandler.kt
@@ -16,10 +16,7 @@ import com.helltar.twitchviewerbot.commands.CommandExecutor.Companion.COMMAND_LI
 import com.helltar.twitchviewerbot.commands.CommandExecutor.Companion.COMMAND_PRIVACY
 import com.helltar.twitchviewerbot.commands.CommandExecutor.Companion.COMMAND_SCREENSHOT
 import com.helltar.twitchviewerbot.commands.CommandExecutor.Companion.COMMAND_START
-import com.helltar.twitchviewerbot.commands.simple.AboutCommand
-import com.helltar.twitchviewerbot.commands.simple.HelpCommand
-import com.helltar.twitchviewerbot.commands.simple.PrivacyCommand
-import com.helltar.twitchviewerbot.commands.simple.StartCommand
+import com.helltar.twitchviewerbot.commands.simple.*
 import com.helltar.twitchviewerbot.commands.system.UpdatePrivacyPolicy
 import com.helltar.twitchviewerbot.commands.twitch.*
 import com.helltar.twitchviewerbot.commands.twitch.keyboard.KeyboardBundle
@@ -35,18 +32,18 @@ class TwitchViewerBotHandler(botModuleOptions: BotModuleOptions) : BotHandler(bo
 
     init {
         commandRegistry.run {
-            register(SimpleCommand("/add") { commandExecutor.execute(AddCommand(it), COMMAND_ADD) })
-            register(SimpleCommand("/clip") { commandExecutor.execute(ClipCommand(it), COMMAND_CLIP) })
-            register(SimpleCommand("/live") { commandExecutor.execute(LiveCommand(it), COMMAND_LIVE) })
-            register(SimpleCommand("/screen") { commandExecutor.execute(ScreenCommand(it), COMMAND_SCREENSHOT) })
-            register(SimpleCommand("/list") { commandExecutor.execute(ListCommand(it), COMMAND_LIST) })
+            register(SimpleCommand("/add") { commandExecutor.executeCommand(AddCommand(it), COMMAND_ADD) })
+            register(SimpleCommand("/clip") { commandExecutor.executeCommand(ClipCommand(it), COMMAND_CLIP) })
+            register(SimpleCommand("/live") { commandExecutor.executeCommand(LiveCommand(it), COMMAND_LIVE) })
+            register(SimpleCommand("/screen") { commandExecutor.executeCommand(ScreenCommand(it), COMMAND_SCREENSHOT) })
+            register(SimpleCommand("/list") { commandExecutor.executeCommand(ListCommand(it), COMMAND_LIST) })
 
-            register(SimpleCommand("/start") { commandExecutor.execute(StartCommand(it), COMMAND_START) })
-            register(SimpleCommand("/help") { commandExecutor.execute(HelpCommand(it), COMMAND_START) })
-            register(SimpleCommand("/about") { commandExecutor.execute(AboutCommand(it), COMMAND_ABOUT) })
+            register(SimpleCommand("/start") { commandExecutor.executeCommand(StartCommand(it), COMMAND_START) })
+            register(SimpleCommand("/help") { commandExecutor.executeCommand(HelpCommand(it), COMMAND_START) })
+            register(SimpleCommand("/about") { commandExecutor.executeCommand(AboutCommand(it), COMMAND_ABOUT) })
 
-            register(SimpleCommand("/updateprivacy") { commandExecutor.execute(UpdatePrivacyPolicy(it), COMMAND_PRIVACY) })
-            register(SimpleCommand("/privacy") { commandExecutor.execute(PrivacyCommand(it), COMMAND_PRIVACY) })
+            register(SimpleCommand("/updateprivacy") { commandExecutor.executeCommand(UpdatePrivacyPolicy(it), COMMAND_PRIVACY) })
+            register(SimpleCommand("/privacy") { commandExecutor.executeCommand(PrivacyCommand(it), COMMAND_PRIVACY) })
 
             registerBundle(KeyboardBundle())
         }
