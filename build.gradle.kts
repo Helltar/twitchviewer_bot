cac6a94ccdb4be011ffe9cc5bfeb8950b9b41f78
Helltar <iam@helltar.com>
2025-03-06 23:05:34 +0200

    update Kotlin, Exposed, and project version; adjust dependencies order

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,31 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
 group = "com.helltar"
-version = "0.9.5"
+version = "0.9.6"
 
 repositories {
     mavenCentral()
 }
 
 object Versions {
-    const val EXPOSED = "0.58.0"
+    const val EXPOSED = "0.60.0"
 }
 
 dependencies {
-    implementation("com.annimon:tgbots-module:8.0.0") {
-        exclude("org.telegram", "telegrambots-webhook")
-    }
-
+    implementation("com.annimon:tgbots-module:8.0.0") { exclude("org.telegram", "telegrambots-webhook") }
     implementation("com.github.twitch4j:twitch4j:1.24.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.10.1")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
+    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:${Versions.EXPOSED}")
     implementation("org.jetbrains.exposed:exposed-core:${Versions.EXPOSED}")
     implementation("org.jetbrains.exposed:exposed-java-time:${Versions.EXPOSED}")
 
-    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:${Versions.EXPOSED}")
-    runtimeOnly("org.postgresql:postgresql:42.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.10.1")
 
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("ch.qos.logback:logback-classic:1.5.16")
