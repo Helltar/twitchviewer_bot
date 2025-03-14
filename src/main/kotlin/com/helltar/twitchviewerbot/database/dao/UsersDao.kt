f20d9021ffed47e6311febd4641f36b490756b23
Helltar <iam@helltar.com>
2025-03-14 21:25:45 +0200

    refactor users table and dao to use upsert for streamlined inserts and updates

--- src/main/kotlin/com/helltar/twitchviewerbot/database/dao/UsersDao.kt
+++ src/main/kotlin/com/helltar/twitchviewerbot/database/dao/UsersDao.kt
@@ -3,33 +3,31 @@ package com.helltar.twitchviewerbot.database.dao
 import com.helltar.twitchviewerbot.database.Database.dbTransaction
 import com.helltar.twitchviewerbot.database.Database.utcNow
 import com.helltar.twitchviewerbot.database.tables.UsersTable
+import com.helltar.twitchviewerbot.database.tables.UsersTable.firstName
 import com.helltar.twitchviewerbot.database.tables.UsersTable.languageCode
-import org.jetbrains.exposed.sql.insertIgnore
-import org.jetbrains.exposed.sql.update
+import com.helltar.twitchviewerbot.database.tables.UsersTable.updatedAt
+import com.helltar.twitchviewerbot.database.tables.UsersTable.username
+import org.jetbrains.exposed.sql.upsert
 import org.telegram.telegrambots.meta.api.objects.User
 
 class UsersDao {
 
-    suspend fun add(user: User): Boolean = dbTransaction {
+    suspend fun upsert(user: User) = dbTransaction {
         UsersTable
-            .insertIgnore {
+            .upsert(
+                onUpdate = {
+                    it[firstName] = user.firstName
+                    it[username] = user.userName
+                    it[languageCode] = user.languageCode
+                    it[updatedAt] = utcNow()
+                })
+            {
                 it[userId] = user.id
                 it[firstName] = user.firstName
                 it[username] = user.userName
                 it[languageCode] = user.languageCode
                 it[createdAt] = utcNow()
             }
-            .insertedCount > 0
-    }
-
-    suspend fun update(user: User): Boolean = dbTransaction {
-        UsersTable
-            .update({ UsersTable.userId eq user.id }) {
-                it[firstName] = user.firstName
-                it[username] = user.userName
-                it[languageCode] = user.languageCode
-                it[updatedAt] = utcNow()
-            } > 0
     }
 
     suspend fun languageCode(userId: Long): String? = dbTransaction {
--- src/main/kotlin/com/helltar/twitchviewerbot/database/tables/UsersTable.kt
+++ src/main/kotlin/com/helltar/twitchviewerbot/database/tables/UsersTable.kt
@@ -1,16 +1,17 @@
 package com.helltar.twitchviewerbot.database.tables
 
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.timestamp
 
-object UsersTable : IntIdTable() {
+object UsersTable : Table() {
 
-    val userId = long("user_id").uniqueIndex()
-
+    val userId = long("user_id")
     val firstName = varchar("first_name", 64)
     val username = varchar("username", 32).nullable()
     val languageCode = varchar("language_code", 20).nullable()
 
     val updatedAt = timestamp("updated_at").nullable()
     val createdAt = timestamp("created_at")
+
+    override val primaryKey = PrimaryKey(userId)
 }
